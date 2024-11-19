fc305c4445c714fc02eb8c35f91bd3b5b4f1a6fa
jokerjaas2002 <joelacosta712@gmail.com>
2024-11-19 13:35:34 -0400

    Feat: Change the image

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", "php1_jokerjaas2002");
+if (!$link) {
+die('Error de Conexión (' . mysqli_connect_errno() . ') '
+. mysqli_connect_error());
+}
+
+?>
+
